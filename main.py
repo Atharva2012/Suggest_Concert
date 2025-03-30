5e7073ca7d4990e4176829bdc2ee5bbc5721ed68
Atharva2012 <kulkarniatharva590@gmail.con>
2025-03-30 00:31:53 -0700

    Added comment on line 11 in main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ client_id = '79b91ee5592049fd8d1f7fe678cedd75'
 client_secret = 'bb4517d558854b15a05d6a87a7ab3698'
 redirect_uri = 'http://localhost:5000/callback'
 scope = 'playlist-read-private'
-
+# new comment in code 00:30 on 03/30
 if __name__ == '__main__':
     app.run(debug=True)
