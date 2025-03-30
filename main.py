0cceffba57d5bdea8df37fa5062dd75fa298a004
Atharva2012 <kulkarniatharva590@gmail.con>
2025-03-30 00:26:33 -0700

    Added another line to REDMI file

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+
+client_id = '79b91ee5592049fd8d1f7fe678cedd75'
+client_secret = 'bb4517d558854b15a05d6a87a7ab3698'
+redirect_uri = 'http://localhost:5000/callback'
+scope = 'playlist-read-private'
+
+if __name__ == '__main__':
+    app.run(debug=True)
