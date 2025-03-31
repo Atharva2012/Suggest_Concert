babb9a581f9f8abfb48d70b3cbc3a1fe6b5b1fec
Atharva2012 <kulkarniatharva590@gmail.con>
2025-03-30 17:29:03 -0700

    Added a comment on line 12

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ client_secret = 'bb4517d558854b15a05d6a87a7ab3698'
 redirect_uri = 'http://localhost:5000/callback'
 scope = 'playlist-read-private'
 # new comment in code 00:30 on 03/30
+#new comment added on line 12 at 17:27 on 03/30
 if __name__ == '__main__':
     app.run(debug=True)
